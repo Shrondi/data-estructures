f86ff945926fb2114340d28899e07671bc5ba88e
Carlos Lucena Robles <carlos.lucena022@gmail.com>
2024-06-04 16:29:32 +0200

    Finish Graph Traversals

--- wgraph_traversals/wgraph_traversals_imp.hpp
+++ wgraph_traversals/wgraph_traversals_imp.hpp
@@ -23,7 +23,19 @@ void depth_first_scan(Graph<T, E> &g, Processor &p)
     // Remember: set the visited flags to false.
     // Remember: to mark the start of the traversal, 'u' arriving
     // from 'u' must be indicated.
+    g.reset(false);
+    g.goto_first_vertex();
 
+    while(g.has_current_vertex()){
+        auto u = g.current_vertex();
+
+        if (!u->is_visited()){
+            depth_first_scan(g, u, u, p);
+            g.goto_vertex(u);
+        }
+
+        g.goto_next_vertex();
+    }
     //
 }
 
@@ -37,6 +49,22 @@ void depth_first_scan(Graph<T, E> &g,
     // TODO
     // Remember: move vertex cursor to v to scan all the edges incident on it.
 
+    v->set_visited(true);
+    p(v,u);
+
+    g.goto_vertex(v);
+    while (g.has_current_edge()) {
+
+        auto u = g.current_edge()->other(v);
+
+        if (!u->is_visited()) {
+            depth_first_scan(g, u, v, p);
+            g.goto_edge(v,u);
+        }
+
+        g.goto_next_edge();
+    }
+
     //
 }
 
@@ -45,7 +73,19 @@ void breadth_first_scan(Graph<T, E> &g, Processor &p)
 {
     // TODO
     // Remember: reset to false the visited flags.
+    g.reset(false);
+    g.goto_first_vertex();
 
+    while(g.has_current_vertex()){
+        auto u = g.current_vertex();
+
+        if (!u->is_visited()){
+            depth_first_scan(g, u, u, p);
+            g.goto_vertex(u);
+        }
+
+        g.goto_next_vertex();
+    }
     //
 }
 
@@ -60,6 +100,34 @@ void breadth_first_scan(Graph<T, E> &g, typename Graph<T, E>::VertexRef &v,
     // Remember: we want to know from which vertex 'u' arriving from when processing
     // a vertex 'v' so we can enqueue a pair <v,u>.
     // Remember: to mark the first vertex we enqueue the pair <u,u>
+    using Pair = std::pair<typename Graph<T, E>::VertexRef, typename Graph<T, E>::VertexRef>;
+
+    std::queue<Pair> q;
+
+    q.push(Pair(g.current_vertex(), g.current_vertex()));
+    while (!q.empty()){
+
+        auto pair = q.front(); 
+        auto u = pair.first;
+        q.pop();
+
+        if (!u->is_visited()){
+            p(pair.first, pair.second);
+            u->set_visited(true);
+            g.goto_vertex(u);
+
+            while (g.has_current_edge()){
+                auto v = g.current_edge()->other(u);
+
+                if (!v->is_visited()){
+                    q.push(Pair(v,u));
+                }
+                g.goto_next_edge();
+            }
+        }
+
+    }
+
 
     //
 }
@@ -80,6 +148,22 @@ void topological_sorting(Graph<T, E> &g,
     // TODO
     // Remember: move vertex cursor to u to scan all the edges incident on it.
 
+    g.goto_vertex(u);
+
+    while (g.has_current_edge()){
+        auto v = g.current_edge()->other(u);
+
+        if (!v->is_visited()){
+            topological_sorting(g, v, sorting);
+            g.goto_edge(u , v);
+        }
+        
+        g.goto_next_edge();
+    }
+
+    u->set_visited(true);
+    sorting.push_front(u);
+
     //
 }
 
@@ -90,6 +174,20 @@ void topological_sorting(Graph<T, E> &g,
     assert(g.is_directed());
     // TODO
     // Remember: set the visited flags to false.
+
+    g.reset(false);
+    g.goto_first_vertex();
+
+    while(g.has_current_vertex()){
+        auto u = g.current_vertex();
+
+        if (!u->is_visited()){
+            topological_sorting(g, u, sorting);
+            g.goto_vertex(u);
+        }
+
+        g.goto_next_vertex();
+    }
 
     //
 }
